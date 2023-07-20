d5cb968303dd2b9496ca8259ce6aa24b1578bd64
Terry <antonypaco12@gmail.com>
2023-07-20 18:58:27 -0500

    Se agrego componente tres

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import C01component from './Componentes/C01component';
+import C02contador from './Componentes/C02contador';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>Componente 1</h1>
+     <C01component/>
+
+     <h2>Componente 2</h2>
+     <C02contador/>
+
+     <h3>Componente 3</h3>
+     <C03dobleestado/>
+
     </div>
   );
 }
